e953f7724d8f0863e934265460cf0f31f08f7b12
Ethan Walker <ethanwalker23081@gmail.com>
2022-04-19 15:25:36 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Roguelike-Gosu-Ruby
+ A roguelike game in Ruby using the game engine Gosu following the guide from rogueliketutorials.com which used python/tcod
---
+++ init.rb
@@ -0,0 +1,12 @@
+require 'gosu'
+require './lib/map'
+require './lib/player'
+require './lib/tile'
+require './lib/main_scene'
+require './lib/inventory'
+require './lib/item'
+require './lib/leaf'
+require './lib/button_handler'
+require './lib/messager'
+
+window = GameWindow.new.show
